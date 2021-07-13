71784afd090098188d59bdb2b5694a37e4683c86
Hquart <hquart@icloud.com>
2021-07-13 16:49:24 +0200

    removed gitignore

---
+++ Baluchon2/APIKeys.swift
@@ -0,0 +1,20 @@
+//
+//  APIKeys.swift
+//  Baluchon2
+//
+//  Created by Naji Achkar on 20/06/2021.
+//
+
+
+import Foundation
+
+struct APIKeys {
+    static let fixer = "xxxxxxxxxxxxxxxxxxx"
+    static let google = "xxxxxxxxxxxxxxxxxxx"
+    static let openWeather = "xxxxxxxxxxxxxxxxxxx"
+}
+
+
+
+
+
